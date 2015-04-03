164a16bc05d9fb6626a2c0b6518665a81ea50f7b
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2015-04-03 12:15:29 +0100

    Added basic User repository and tests.

---
+++ src/main/java/uk/co/taidev/projects/twittertracking/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package uk.co.taidev.projects.twittertracking.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uk.co.taidev.projects.twittertracking.models.User;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID>{
+}
---
+++ src/test/java/uk/co/taidev/projects/twittertracking/repository/UserRepositoryTest.java
@@ -0,0 +1,41 @@
+package uk.co.taidev.projects.twittertracking.repository;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import uk.co.taidev.projects.twittertracking.TwitterTrackingApplication;
+import uk.co.taidev.projects.twittertracking.models.User;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = TwitterTrackingApplication.class)
+@ActiveProfiles({"dev"})
+public class UserRepositoryTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserRepositoryTest.class);
+
+
+    @Autowired
+    private UserRepository subject;
+
+
+    @Test
+    public void validUserPersists() {
+        User user = new User("John", "Doe");
+
+        subject.save(user);
+
+        assertThat(user.getId(), is(notNullValue()));
+        LOGGER.info("User saved {}", user);
+    }
+
+}
